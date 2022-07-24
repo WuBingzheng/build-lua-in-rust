4fea75b70d1bfa026650f9fd03aa5e7811628964
wub <wubingzheng@gmail.com>
2022-07-24 16:08:10 +0800

    add minimal interpreter for helloworld

---
+++ listing/ch01.hello_world/src/parse.rs
@@ -0,0 +1,41 @@
+use std::fs::File;
+use crate::lex::{Lex, Token};
+use crate::bytecode::ByteCode;
+use crate::value::Value;
+
+#[derive(Debug)]
+pub struct ParseProto {
+    pub constants: Vec::<Value>,
+    pub byte_codes: Vec::<ByteCode>,
+}
+
+impl ParseProto {
+    pub fn load(input: File) -> Self {
+        let mut constants = Vec::new();
+        let mut byte_codes = Vec::new();
+        let mut lex = Lex::new(input);
+
+        loop {
+            match lex.next() {
+                Token::Name(name) => { // `Name LiteralString` as function call
+                    constants.push(Value::String(name));
+                    byte_codes.push(ByteCode::GetGlobal(0, (constants.len()-1) as u8));
+
+                    if let Token::String(s) = lex.next() {
+                        constants.push(Value::String(s));
+                        byte_codes.push(ByteCode::LoadConst(1, (constants.len()-1) as u8));
+                        byte_codes.push(ByteCode::Call(0));
+                    } else {
+                        panic!("expected string");
+                    }
+                }
+                Token::Eos => break,
+                t => panic!("unexpected token: {t:?}"),
+            }
+        }
+
+        println!("[PARSE] constants: {:?}", &constants);
+        println!("[PARSE] byte_codes: {:?}", &byte_codes);
+        ParseProto { constants, byte_codes }
+    }
+}
---
+++ listing/ch01.hello_world/test_lua/hello2.lua
@@ -0,0 +1,3 @@
+print "hello, world!"
+print "hello, again!"
+print "haha!"
